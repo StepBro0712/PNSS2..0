466542cca76e86ddb93f77cad4e9fed154e8dc5a
stepa <mailstepbro@mail.ru>
2023-09-21 19:14:58 +0700

    1

---
+++ config/app.php
@@ -0,0 +1,30 @@
+<?php
+return [
+    //Класс аутентификации
+    'auth' => \Src\Auth\Auth::class,
+    //Клас пользователя
+    'identity' => \Model\User::class,
+    //Классы для middleware
+    'routeMiddleware' => [
+        'auth' => \Middlewares\AuthMiddleware::class,
+        'role' => \Middlewares\RoleMiddleware::class,
+    ],
+    'validators' => [
+        'required' => \Validators\RequireValidator::class,
+        'unique' => \Validators\UniqueValidator::class,
+        'cyrillic' => \Validators\CyrillicValidator::class,
+        'latin' => \Validators\LatinValidator::class,
+    ],
+    'providers' => [
+        'kernel' => \Providers\KernelProvider::class,
+        'route' => \Providers\RouteProvider::class,
+        'db' => \Providers\DBProvider::class,
+        'auth' => \Providers\AuthProvider::class,
+    ],
+    'routeAppMiddleware' => [
+        'csrf' => \Middlewares\CSRFMiddleware::class,
+        'trim' => \Middlewares\TrimMiddleware::class,
+        'specialChars' => \Middlewares\SpecialCharsMiddleware::class,
+        'json' => \Middlewares\JSONMiddleware::class,
+    ]
+];
